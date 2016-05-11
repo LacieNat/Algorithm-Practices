11b5a2054dd20a23cdda1ea62b103cef0e00327d
Lacie Fan Yuxin <lacie@velocloud.net>
2016-05-11 18:10:14 +0800

    Andy's first dictionary soln

--- uva/10815 Andy's First Dictionary/main.cpp
+++ uva/10815 Andy's First Dictionary/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <string>
-
+#include <set>
+#include <algorithm>
+#include <sstream>
 using namespace std;
 
 int main() {
 	string s;
-
+	set<string> dictionary;
+	stringstream ss;
 	while(cin >> s) {
-		
+		for(int i=0; i<s.length(); i++) {
+			if(isalpha(s[i])) {
+				s[i] = ::tolower(s[i]);
+			} else {
+				s[i] = ' ';
+			}
+		}
+
+		ss.clear();
+		ss << s;
+
+		while (ss >> s) dictionary.insert(s);
+
 	}
+	for(set<string>::iterator it = dictionary.begin(); it!=dictionary.end(); ++it)
+		cout<<*it<<endl;
+
+	return 0;
 	
 }

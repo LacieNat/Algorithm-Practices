82eea639fab035b4b32c04153a5b48351e4eaeb7
Lacie Fan Yuxin <lacie@velocloud.net>
2016-05-13 15:49:09 +0800

    progress

---
+++ uva/11136 Hoax or what/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	int days;
+	int bills;
+	while (cin >> days) {
+		for(int i=0; i<days; i++) {
+			cin >> bills;
+
+			for(int j=0; j<bills; j++) {
+				
+
+			}
+		}
+	}
+}

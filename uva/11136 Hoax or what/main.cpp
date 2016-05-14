67097a202b7efb8ff1909c3b245d972d37ee0381
Lacie Fan Yuxin <lacie@velocloud.net>
2016-05-14 16:09:36 +0800

    hoax or what

--- uva/11136 Hoax or what/main.cpp
+++ uva/11136 Hoax or what/main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+#include <set>
+
+using namespace std;
 
 int main() {
 	int days;
 	int bills;
-	while (cin >> days) {
+	int bill;
+	
+	cin >> days;
+	multiset<int> s;
+	while (days) {
+		long sum = 0;
 		for(int i=0; i<days; i++) {
 			cin >> bills;
 
 			for(int j=0; j<bills; j++) {
-				
-
+				cin >> bill;
+				s.insert(bill);
 			}
+			
+			multiset<int>::iterator low = s.begin();
+			multiset<int>::iterator hi = s.end();
+			--hi;
+			sum += (*hi - *low);
+			s.erase(low);
+			s.erase(hi);
 		}
+		
+		cout << sum <<endl;
+		cin >> days;
+		s.clear();
 	}
+
+	return 0;
 }

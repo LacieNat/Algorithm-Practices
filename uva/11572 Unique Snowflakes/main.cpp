4c42737b31ff558c11bd52ec82a9a49165677461
Lacie Fan Yuxin <lacie@velocloud.net>
2016-08-30 23:19:47 +0800

    Unique Snowflakes

--- uva/11572 Unique Snowflakes/main.cpp
+++ uva/11572 Unique Snowflakes/main.cpp
@@ -1,24 +1,60 @@
 #include <iostream>
-#include <unordered_set>
-
-
+#include <unordered_map>
+#include <utility>
+#include <algorithm>
 using namespace std;
+long segStart[1000001];
+long segEnd[1000001];
+unordered_map<long, long> m;
 
 int main() {
 
-	int t, n, flake;
-	unordered_set<int> s;
+	long t, n, flake;
 	cin >> t;
-
-	for(int i=0; i<t; i++) {
+	ios_base::sync_with_stdio(false);
+	
+	for(long i=0; i<t; i++) {
 		cin >> n;
-		s.clear();
+		m.clear();		
 
-		for(int j=0; j<n; j++) {
+		fill_n(segStart, n, -1);
+		fill_n(segEnd, n, -1);
+		for(long j=0; j<n; j++) {
 			cin >> flake;
-			s.insert(flake);
+			if (m.find(flake) != m.end()) {
+				long prev = m.at(flake);
+				segStart[prev] = j;
+				segEnd[j] = prev;
+				m.at(flake) = j;
+			} else {
+				m.insert(pair<long,long>(flake, j));
+			}
 		}
 		
-		cout << s.size() << endl;
+		long maxlen = 0;
+	    for(long j=0; j<n; j++) {
+			long end = segStart[j];
+			long len;
+
+			if(end == -1) {
+				segStart[j] = n;
+				
+			} 
+			
+			len = segStart[j] - j;	
+			
+			for(long k=j+1; k<segStart[j]; k++) {
+				if(segEnd[k]!= -1 && segEnd[k]>j) {
+					len = k-j;
+					break;
+				}
+			}
+			maxlen = len>maxlen?len:maxlen;
+
+		}
+		
+		cout << maxlen << endl;
 	}
+
+	return 0;
 }

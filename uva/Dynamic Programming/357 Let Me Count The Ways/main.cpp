f071e143e56c8d69362f8e4a084b485f9ab79ed5
Lacie Fan Yuxin <lacie@velocloud.net>
2016-09-29 01:09:11 +0800

    357

--- uva/Dynamic Programming/357 Let Me Count The Ways/main.cpp
+++ uva/Dynamic Programming/357 Let Me Count The Ways/main.cpp
@@ -4,9 +4,27 @@
 
 using namespace std;
 
+unsigned long long int dp[30001];
+int c[5] = {1, 5, 10, 25, 50};
+
+
 int main() {
 
-	
+	int n;
+	dp[0] = 1;
+
+	for(int i=0; i<5; i++) {
+        for( int j=c[i]; j<30001; j++) {
+            dp[j] += dp[j-c[i]];
+        }
+    }
+
+	while(cin >> n) {
+		
+		if(dp[n]>1) cout << "There are " << dp[n] << " ways to produce " << n << " cents change."<<endl;
+		else cout << "There is only 1 way to produce "<< n << " cents change." << endl;
+
+	}
 
 
 }
